9d6ebe85404e4c8430a5fed1c48eda884b277d5b
Paulo Diovani Gonçalves <paulo@diovani.com>
2014-04-10 15:47:16 -0300

    Some client adjusts;

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 (function() {
     var client = require('./app.js');
 
+    client.list_sinks(console.log);
     // client.list_sources(console.log);
-    client.start_record('alsa_output.pci-0000_00_1b.0.analog-stereo.monitor');
+    // client.start_record('alsa_output.pci-0000_00_1b.0.analog-stereo.monitor');
 })();
